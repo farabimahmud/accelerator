ccf0b95a638839411f97fdc14f34a027de8ba93f
jyhuang <jyhuang@tamu.edu>
2019-07-14 17:12:27 -0400

    [booksim] callable booksim in python

---
+++ setup_env.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+if [ "$(basename -- "$0")" == "$(basename -- "$BASH_SOURCE")" ]; then
+  echo "Don't run $0, source it" >&2
+  echo "source `basename $BASH_SOURCE` in directory $dir"
+  exit 1
+fi
+
+if [ "$(dirname -- "$(realpath $BASH_SOURCE)")" != "$PWD" ]; then
+  echo "FAIL: source `basename $BASH_SOURCE` in directory $dir"
+  return
+fi
+
+export BOOKSIMSRC=$PWD/src/booksim2/src
+
+echo "source successfully!"
