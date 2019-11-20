7e22a78ed2d3cdc34d378b1cd0bd9ce9a2f1a857
jyhuang <jyhuang@tamu.edu>
2019-11-20 11:07:15 -0500

    [util] runscript update

--- src/run_experiment.sh
+++ src/run_experiment.sh
@@ -7,11 +7,13 @@ for nn in AlphaGoZero DeepSpeech2 FasterRCNN NCF_recommendation \
   Sentimental_seqLSTM_short Transformer Transformer_short
 do
   for allreduce in ring multitree mxnettree
-  screen -dmS ${nn}_${allreduce}
-  screen -S $nn -X stuff $'python simulate.py \
-    --network SCALE-Sim/topologies/mlperf/'$nn'.csv \
-    --run-name '$nn' \
-    --booksim-config booksim2/runfiles/ctorus44multitree.cfg \
-    --allreduce '$allreduce' \
-    --only-allreduce &\n'
+  do
+    screen -dmS ${nn}_${allreduce}
+    screen -S $nn -X stuff $'python simulate.py \
+      --network SCALE-Sim/topologies/mlperf/'$nn'.csv \
+      --run-name '$nn' \
+      --booksim-config booksim2/runfiles/ctorus44multitree.cfg \
+      --allreduce '$allreduce' \
+      --only-allreduce &\n'
+  done
 done
