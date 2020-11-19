a3822781745f8655d749e6624fc726ad0e505675
jyhuang <jyhuang@tamu.edu>
2020-11-18 21:44:51 -0500

    [bigraph] ring construction

--- src/allreduce/network/bigraph.py
+++ src/allreduce/network/bigraph.py
@@ -1,5 +1,6 @@
 import argparse
 import numpy as np
+from copy import deepcopy
 
 from network import Network
 
@@ -43,6 +44,47 @@ class BiGraph(Network):
                 print('      connects to switches {}'.format(self.switch_to_switch[sw]))
                 print('      connects to nodes {}'.format(self.switch_to_node[sw]))
 
+        self.ring = [0]
+        for i in range(self.m):
+            for j in range(self.n // 2):
+                up_sw = self.ring[-1] // self.m
+                down_sw = (up_sw + i) % (self.n // 2) + (self.n // 2)
+                self.ring.append(down_sw * self.m + i)
+                next_up_sw = (down_sw - self.n // 2 + i + 1) % (self.n // 2)
+                if i == (self.m - 1) and j == (self.n // 2 - 1):
+                    break
+                if j == (self.n // 2 - 1):
+                    self.ring.append(next_up_sw * self.m + i + 1)
+                else:
+                    self.ring.append(next_up_sw * self.m + i)
+
+        switches = []
+        for i in self.ring:
+            switches.append(i // self.m)
+        print('ring: {}'.format(self.ring))
+        print('switches in ring: {}'.format(switches))
+
+        node_to_switch = deepcopy(self.node_to_switch)
+        switch_to_switch = deepcopy(self.switch_to_switch)
+        switch_to_node = deepcopy(self.switch_to_node)
+
+        # verify the ring with topology
+        current = 0
+        while current < self.nodes:
+            current_node = self.ring[current]
+            next_node = self.ring[(current + 1) % self.nodes]
+            cur_sw = self.node_to_switch[current_node][0]
+            node_to_switch[current_node] = None
+            next_sw = self.node_to_switch[next_node][0]
+            switch_to_switch[cur_sw].remove(next_sw)
+            switch_to_node[next_sw].remove(next_node)
+            current += 1
+
+        #print('node-to-switch: {}'.format(node_to_switch))
+        #print('switch-to-switch: {}'.format(switch_to_switch))
+        #print('switch-to-node: {}'.format(switch_to_node))
+
+        '''
         # ring for 32 nodes with m = 4 and n = 8
         # [0, 16, 4, 20, 8, 24, 12, 28, 1, 21, 13, 17, 9, 29, 5, 25, 10, 22
         #  6, 18, 2, 30, 14, 26, 7, 31, 11, 19, 15, 23, 3, 27]
@@ -70,6 +112,7 @@ class BiGraph(Network):
             #print('node-to-switch: {}'.format(node_to_switch))
             #print('switch-to-switch: {}'.format(switch_to_switch))
             #print('switch-to-node: {}'.format(switch_to_node))
+        '''
     # def build_graph(self, filename=None)
 
 
