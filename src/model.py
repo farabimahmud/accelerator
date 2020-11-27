34c133f629b4c26e9273a5706c70a9fe16ceda0e
jyhuang <jyhuang@tamu.edu>
2020-11-27 00:38:45 -0500

    [model] bug fix for model layer size index

--- src/model.py
+++ src/model.py
@@ -47,9 +47,9 @@ class Model:
             self.layers[self.num_layers]['num_filters'] = int(elems[6])
             self.layers[self.num_layers]['stride'] = int(elems[7])
 
-            self.num_layers += 1
             layer_size = int(elems[3]) * int(elems[4]) * int(elems[5]) * int(elems[6])
             self.layers[self.num_layers]['size'] = layer_size
+            self.num_layers += 1
 
             self.size += layer_size
 
